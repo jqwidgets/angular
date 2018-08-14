ec47836c311e1737bf101784215c40eef463dedc
www.jQWidgets.com <info@jqwidgets.com>
2018-08-14 18:08:36 +0300

    Initial Commit

--- formattedinput/exponentialnotation/src/app/app.component.ts
+++ formattedinput/exponentialnotation/src/app/app.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component, ViewChild } from '@angular/core';
 
-import { jqxFormattedInputComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxformattedinput';
+import { jqxFormattedInputComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxformattedinput.ts';
 
 @Component({
     selector: 'app-root',
