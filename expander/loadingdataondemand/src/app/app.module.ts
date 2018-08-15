436c789fa9016adbeb303784402779861b806f52
www.jQWidgets.com <info@jqwidgets.com>
2018-08-15 17:02:53 +0300

    Initial Commit

--- expander/loadingdataondemand/src/app/app.module.ts
+++ expander/loadingdataondemand/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; 
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { jqxExpanderComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxexpander.ts';
 
 @NgModule({
     declarations: [AppComponent, jqxExpanderComponent],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, HttpModule],
     providers: [],
     bootstrap: [AppComponent]
 })
