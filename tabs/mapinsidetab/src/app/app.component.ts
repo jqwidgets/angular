b231f78d1968c86cf6c543ec79228d8c6ccb8457
www.jQWidgets.com <info@jqwidgets.com>
2018-08-15 15:40:27 +0300

    Initial Commit

--- tabs/mapinsidetab/src/app/app.component.ts
+++ tabs/mapinsidetab/src/app/app.component.ts
@@ -1,42 +1,33 @@
-﻿import { Component, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+﻿import { Component, ViewChild, ElementRef } from '@angular/core';
 
-import { jqxTabsComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxtabs.ts';
-
-declare let google: any;
+declare const google: any;
 
 @Component({
     selector: 'app-root',
     styleUrls: ['./app.component.css'],
-    templateUrl: './app.component.html',
-    encapsulation: ViewEncapsulation.None
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent {
-    @ViewChild('jqxTabs') jqxTabs: jqxTabsComponent;
     @ViewChild('map') map: ElementRef;
-	
- 	getWidth() : any {
-		if (document.body.offsetWidth < 600) {
-			return '90%';
-		}
-		
-		return 600;
-	}
-    
-    initialize = () => {
-        let directionsService = new google.maps.DirectionsService;
-        let directionsDisplay = new google.maps.DirectionsRenderer;
-        let map = new google.maps.Map(this.map.nativeElement, {
+
+    ngAfterViewInit(): void {
+        const directionsService = new google.maps.DirectionsService;
+        const directionsDisplay = new google.maps.DirectionsRenderer;
+        const map = new google.maps.Map(this.map.nativeElement, {
             zoom: 17,
             center: { lat: 29.979234, lng: 31.134202 },
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
+        
         directionsDisplay.setMap(map);
     }
-
-    initTabContent = (tab) => {
-        if (tab === 0) {
-            google.maps.event.addDomListener(window, 'load', this.initialize);
-        }
-    };
+	
+    getWidth(): any {
+      if (document.body.offsetWidth < 600) {
+        return '90%';
+      }
+      
+      return 600;
+    }
 }
