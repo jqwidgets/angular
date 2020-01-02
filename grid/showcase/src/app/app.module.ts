30af4ce4ce5ac6de172a9da7734a320f06309484
jQWidgets <info@jqwidgets.com>
2020-01-02 19:14:03 +0200

    Update app.module.ts

--- grid/showcase/src/app/app.module.ts
+++ grid/showcase/src/app/app.module.ts
@@ -1,20 +1,16 @@
-﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { jqxGridModule } from 'jqwidgets-ng/jqxgrid';
-import { jqxButtonModule } from 'jqwidgets-ng/jqxbuttons';
+import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid.ts';
 
 @NgModule({
-  declarations: [
-      AppComponent
-  ],
-  imports: [
-      BrowserModule, CommonModule, jqxGridModule, jqxButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, jqxGridComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
+        
