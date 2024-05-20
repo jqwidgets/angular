6111fbb6805f4e2a2cf55a034a08822c2bf6c88f
bmarkov <boikom@jqwidgets.com>
2024-05-20 08:52:42 +0300

    timeline component

---
+++ timeline/defaultfunctionality/src/app/app.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+
+import { jqxTimelineComponent } from 'jqwidgets-ng/jqxtimeline';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+	encapsulation: ViewEncapsulation.None,
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    @ViewChild('myTimeline', { static: false }) myTimeline: jqxTimelineComponent;
+	
+	dataSource = [
+	 { date: "May 15, 2024", description: "Flight: Reserving airline tickets", subtitle: "May 15, 2024", title: "Flight Reservation", icon: "material-icons flight", dotCSS: "" },
+	 { date: "June 22, 2024", description: "Hotel: Booking for the trip", subtitle: "June 22, 2024", title: "Booking", icon: "material-icons hotel", dotCSS: "" },
+	 { date: "July 12, 2024", description: "Flight: Take off", subtitle: "July 12, 2024", title: "Flight", icon: "material-icons flight-takeoff", dotCSS: "" },
+   	 { date: "July 13, 2024", description: "Excursion Plans: Hiking", subtitle: "July 13, 2024", title: "Plans", icon: "material-icons hiking", dotCSS: "" }
+	]
+}
